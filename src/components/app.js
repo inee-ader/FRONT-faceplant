88cd0722816c948d06f1839fa16673844e089252
xoliio <xoliio@gmail.com>
2021-01-14 15:25:04 -0800

    troubleshooting cors

--- src/components/app.js
+++ src/components/app.js
@@ -15,7 +15,7 @@ export default class App extends Component {
   }
 
   checkLoginStatus = () => {
-    axios.get(DEPLOYED, { withCredentials: true })
+    axios.get('https://mighty-wildwood-93362.herokuapp.com', { withCredentials: true })
     .then(response => {  
       if(response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN"){
         this.setState({
