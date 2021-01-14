a42486b5162564e2eb44a07f9d3389a2ef96b3c9
xoliio <xoliio@gmail.com>
2021-01-14 10:19:11 -0800

    user create

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Registration from './auth/Registration'; 
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <h1> HOME </h1>
+                <Registration />
             </div>
         );
     }
---
+++ src/components/auth/Registration.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class Registration extends Component {
+    state = {
+        name: '',
+        email: '', 
+        password: '', 
+        password_confirmation: '', 
+        username: '',
+        headline: '',
+        registrationErrors: ''
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        const {name, email, password, password_confirmation, username, headline} = this.state
+
+        axios.post('http://localhost:3001/registrations', {
+            user: {
+                name: name,
+                email: email, 
+                password: password, 
+                password_confirmation: password_confirmation, 
+                username: username, 
+                headline: headline
+            }
+        }, 
+        { withCredentials: true }
+        ).then(response => {
+            console.log("registration response", response)
+        }).catch(error => {
+            console.log("registration error", error)
+        })
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>REGISTRATION</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        name="name"
+                        placeholder="Sally Sue"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                        required
+                    />
+                    <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="me@email.com" 
+                        value={this.state.email} 
+                        onChange={this.handleChange} 
+                        required 
+                    />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        placeholder="Password" 
+                        value={this.state.password} 
+                        onChange={this.handleChange} 
+                        required 
+                    />
+                    <input 
+                        type="password" 
+                        name="password_confirmation" 
+                        placeholder="confirm password" 
+                        value={this.state.password_confirmation} 
+                        onChange={this.handleChange} 
+                        required 
+                    />
+                    <input 
+                        name="username"
+                        placeholder="PlantLover69"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                        required
+                    />
+                    <button type="submit">Register</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Registration;
